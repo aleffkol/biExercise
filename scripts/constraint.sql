ec34f41c336d21217f9de13827892a75b8059e96
aleffkol <36783131+aleffkol@users.noreply.github.com>
2020-02-17 16:24:20 -0300

    Add files via upload

---
+++ scripts/constraint.sql
@@ -0,0 +1,19 @@
+/*Constraint foreign key (detalhes pedidos - produtos)*/
+alter table detalhespedido
+add constraint fk_dp_pro foreign key (códigodoproduto) references produtos(códigodoproduto)
+/*Constraint foreign key (produtos - fornecedores)*/
+alter table produtos
+add constraint fk_pro_for foreign key (códigodofornecedor) references fornecedores(códigodofornecedor)
+/*Constraint foreign key (produtos - categoria)*/
+alter table produtos
+add constraint fk_cat_pro foreign key (códigodacategoria) references categorias(códigodacategoria)
+/*Constraint foreign key (pedidos - cliente)*/
+alter table pedidos
+add constraint fk_ped_cli foreign key (códigodocliente) references clientes(códigodocliente)
+/*Constraint foreign key (pedidos - funcionários)*/
+alter table pedidos
+add constraint fk_ped_fun foreign key (códigodofuncionário) references funcionarios(códigodofuncionário)
+/*Constraint foreign key (funcionários - supervisor)*/
+alter table funcionarios
+add constraint fk_fun_sup foreign key (supervisor) references funcionarios(códigodofuncionário)
+
